2bc1d58dea6b05d6cab848df0d8c9eadb5db889f
ivoviz <viz.ivo@gmail.com>
2021-10-07 17:46:26 +0200

    chore: release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import users from '../assets/payouts.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tvicsiriccs';
+export class AppComponent implements AfterViewInit {
+
+  displayedColumns: string[] = [
+    'id',
+    'display_name',
+    'ad_share_gross',
+    'sub_share_gross',
+    'bits_share_gross',
+    'bits_extension_share_gross',
+    'prime_sub_share_gross',
+    'bit_share_ad_gross',
+    'experimental_rev_gross',
+    'sum',
+    'avg'
+  ];
+  dataSource = new MatTableDataSource(users);
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor() {
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+}
+
+export interface User {
+  id: number;
+  display_name: string;
+  ad_share_gross: number;
+  sub_share_gross: number;
+  bits_share_gross: number;
+  bits_developer_share_gross: number;
+  bits_extension_share_gross: number;
+  prime_sub_share_gross: number;
+  bit_share_ad_gross: number;
+  fuel_rev_gross: number;
+  bb_rev_gross: number;
+  experimental_rev_gross: number;
+  sum: number;
+  avg: number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
